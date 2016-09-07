b2a5f06748d2e5eef5bfeff578eb4d4774853227
lili hualtibamba <lilihperezz@gmail.com>
2016-09-07 12:59:46 -0500

    ejercicio 4

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 
 window.addEventListener("load", function() {
-    var button = document.getElementById("calcular");
-    button.addEventListener("click", function() {
-        var segundos = parseInt(document.getElementById("segundos").value);
-   
-	    setInterval (color,segundos*1000);
-	    function color(){
-	    document.body.style.backgroundColor= '#'+Math.floor(Math.random()*16777215).toString(16);
-		}
-	});
+   var button = document.getElementById("calcular");
+   button.addEventListener("click", function() {
+       var segundos = parseInt(document.getElementById("segundos").value);
+       var numero= parseInt(document.getElementById("numero").value);
+
+       contador(segundos,numero);
+    });
+    function contador(segundos, numero) {
+        setInterval(function(){document.write(numero++)}, segundos*1000);
+    }
 });
-
-
  
